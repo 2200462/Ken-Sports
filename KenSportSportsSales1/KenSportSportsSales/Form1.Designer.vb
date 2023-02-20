32e67a85d5c60b125e6ece40ac7dfa1eaf3da257
2200462 <108864846+2200462@users.noreply.github.com>
2023-02-20 11:07:02 +0300

    Add files via upload

---
+++ KenSportSportsSales1/KenSportSportsSales/Form1.Designer.vb
@@ -0,0 +1,383 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.BTNExit = New System.Windows.Forms.Button()
+        Me.TXTTax = New System.Windows.Forms.TextBox()
+        Me.TXTTotalsales = New System.Windows.Forms.TextBox()
+        Me.TXTSubtotal = New System.Windows.Forms.TextBox()
+        Me.TXTThirdprice = New System.Windows.Forms.TextBox()
+        Me.TXTSecondprice = New System.Windows.Forms.TextBox()
+        Me.TXTfirstprice = New System.Windows.Forms.TextBox()
+        Me.BTNcheckout = New System.Windows.Forms.Button()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.TXTBasketballs = New System.Windows.Forms.TextBox()
+        Me.TXTVolleyballs = New System.Windows.Forms.TextBox()
+        Me.TXTTotalitems = New System.Windows.Forms.TextBox()
+        Me.TXTFootballs = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.Gray
+        Me.GroupBox1.Controls.Add(Me.BTNExit)
+        Me.GroupBox1.Controls.Add(Me.TXTTax)
+        Me.GroupBox1.Controls.Add(Me.TXTTotalsales)
+        Me.GroupBox1.Controls.Add(Me.TXTSubtotal)
+        Me.GroupBox1.Controls.Add(Me.TXTThirdprice)
+        Me.GroupBox1.Controls.Add(Me.TXTSecondprice)
+        Me.GroupBox1.Controls.Add(Me.TXTfirstprice)
+        Me.GroupBox1.Controls.Add(Me.BTNcheckout)
+        Me.GroupBox1.Controls.Add(Me.Label14)
+        Me.GroupBox1.Controls.Add(Me.Label13)
+        Me.GroupBox1.Controls.Add(Me.Label12)
+        Me.GroupBox1.Controls.Add(Me.Label11)
+        Me.GroupBox1.Controls.Add(Me.Label10)
+        Me.GroupBox1.Controls.Add(Me.Label9)
+        Me.GroupBox1.Controls.Add(Me.TXTBasketballs)
+        Me.GroupBox1.Controls.Add(Me.TXTVolleyballs)
+        Me.GroupBox1.Controls.Add(Me.TXTTotalitems)
+        Me.GroupBox1.Controls.Add(Me.TXTFootballs)
+        Me.GroupBox1.Controls.Add(Me.Label8)
+        Me.GroupBox1.Controls.Add(Me.Label7)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Font = New System.Drawing.Font("Times New Roman", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(760, 471)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Sports Sales"
+        '
+        'BTNExit
+        '
+        Me.BTNExit.BackColor = System.Drawing.Color.Red
+        Me.BTNExit.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.BTNExit.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.BTNExit.Font = New System.Drawing.Font("Times New Roman", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BTNExit.Location = New System.Drawing.Point(465, 376)
+        Me.BTNExit.Name = "BTNExit"
+        Me.BTNExit.Size = New System.Drawing.Size(91, 52)
+        Me.BTNExit.TabIndex = 25
+        Me.BTNExit.Text = "Exit"
+        Me.BTNExit.UseVisualStyleBackColor = False
+        '
+        'TXTTax
+        '
+        Me.TXTTax.Location = New System.Drawing.Point(449, 270)
+        Me.TXTTax.Name = "TXTTax"
+        Me.TXTTax.Size = New System.Drawing.Size(122, 29)
+        Me.TXTTax.TabIndex = 24
+        '
+        'TXTTotalsales
+        '
+        Me.TXTTotalsales.Location = New System.Drawing.Point(449, 319)
+        Me.TXTTotalsales.Name = "TXTTotalsales"
+        Me.TXTTotalsales.Size = New System.Drawing.Size(122, 29)
+        Me.TXTTotalsales.TabIndex = 23
+        '
+        'TXTSubtotal
+        '
+        Me.TXTSubtotal.Location = New System.Drawing.Point(449, 233)
+        Me.TXTSubtotal.Name = "TXTSubtotal"
+        Me.TXTSubtotal.Size = New System.Drawing.Size(122, 29)
+        Me.TXTSubtotal.TabIndex = 22
+        '
+        'TXTThirdprice
+        '
+        Me.TXTThirdprice.Font = New System.Drawing.Font("Times New Roman", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TXTThirdprice.Location = New System.Drawing.Point(449, 191)
+        Me.TXTThirdprice.Name = "TXTThirdprice"
+        Me.TXTThirdprice.Size = New System.Drawing.Size(122, 29)
+        Me.TXTThirdprice.TabIndex = 21
+        '
+        'TXTSecondprice
+        '
+        Me.TXTSecondprice.Font = New System.Drawing.Font("Times New Roman", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TXTSecondprice.Location = New System.Drawing.Point(449, 146)
+        Me.TXTSecondprice.Name = "TXTSecondprice"
+        Me.TXTSecondprice.Size = New System.Drawing.Size(122, 29)
+        Me.TXTSecondprice.TabIndex = 20
+        '
+        'TXTfirstprice
+        '
+        Me.TXTfirstprice.Font = New System.Drawing.Font("Times New Roman", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TXTfirstprice.Location = New System.Drawing.Point(449, 105)
+        Me.TXTfirstprice.Name = "TXTfirstprice"
+        Me.TXTfirstprice.Size = New System.Drawing.Size(122, 29)
+        Me.TXTfirstprice.TabIndex = 19
+        '
+        'BTNcheckout
+        '
+        Me.BTNcheckout.BackColor = System.Drawing.Color.Blue
+        Me.BTNcheckout.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.BTNcheckout.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.BTNcheckout.Font = New System.Drawing.Font("Times New Roman", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BTNcheckout.Location = New System.Drawing.Point(341, 376)
+        Me.BTNcheckout.Name = "BTNcheckout"
+        Me.BTNcheckout.Size = New System.Drawing.Size(91, 52)
+        Me.BTNcheckout.TabIndex = 18
+        Me.BTNcheckout.Text = "CheckOut"
+        Me.BTNcheckout.UseVisualStyleBackColor = False
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Font = New System.Drawing.Font("Times New Roman", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label14.Location = New System.Drawing.Point(341, 331)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(79, 17)
+        Me.Label14.TabIndex = 17
+        Me.Label14.Text = "Total Sales:"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Font = New System.Drawing.Font("Times New Roman", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label13.Location = New System.Drawing.Point(341, 241)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(66, 17)
+        Me.Label13.TabIndex = 16
+        Me.Label13.Text = "SubTotal:"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Font = New System.Drawing.Font("Times New Roman", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label12.Location = New System.Drawing.Point(341, 282)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(95, 17)
+        Me.Label12.TabIndex = 15
+        Me.Label12.Text = "Tax: @ 5.00%"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Font = New System.Drawing.Font("Times New Roman", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label11.Location = New System.Drawing.Point(341, 154)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(72, 17)
+        Me.Label11.TabIndex = 14
+        Me.Label11.Text = "Ksh. 49.00"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Times New Roman", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.Location = New System.Drawing.Point(341, 198)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(72, 17)
+        Me.Label10.TabIndex = 13
+        Me.Label10.Text = "Ksh. 39.00"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Times New Roman", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(341, 107)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(72, 17)
+        Me.Label9.TabIndex = 12
+        Me.Label9.Text = "Ksh. 44.00"
+        '
+        'TXTBasketballs
+        '
+        Me.TXTBasketballs.Font = New System.Drawing.Font("Times New Roman", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TXTBasketballs.Location = New System.Drawing.Point(145, 155)
+        Me.TXTBasketballs.Name = "TXTBasketballs"
+        Me.TXTBasketballs.Size = New System.Drawing.Size(122, 29)
+        Me.TXTBasketballs.TabIndex = 11
+        '
+        'TXTVolleyballs
+        '
+        Me.TXTVolleyballs.Font = New System.Drawing.Font("Times New Roman", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TXTVolleyballs.Location = New System.Drawing.Point(145, 198)
+        Me.TXTVolleyballs.Name = "TXTVolleyballs"
+        Me.TXTVolleyballs.Size = New System.Drawing.Size(122, 29)
+        Me.TXTVolleyballs.TabIndex = 10
+        '
+        'TXTTotalitems
+        '
+        Me.TXTTotalitems.Location = New System.Drawing.Point(145, 241)
+        Me.TXTTotalitems.Name = "TXTTotalitems"
+        Me.TXTTotalitems.Size = New System.Drawing.Size(122, 29)
+        Me.TXTTotalitems.TabIndex = 9
+        '
+        'TXTFootballs
+        '
+        Me.TXTFootballs.Font = New System.Drawing.Font("Times New Roman", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TXTFootballs.Location = New System.Drawing.Point(145, 107)
+        Me.TXTFootballs.Name = "TXTFootballs"
+        Me.TXTFootballs.Size = New System.Drawing.Size(122, 29)
+        Me.TXTFootballs.TabIndex = 8
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Times New Roman", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(33, 241)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(83, 17)
+        Me.Label8.TabIndex = 7
+        Me.Label8.Text = "Total Items:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Times New Roman", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(33, 203)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(81, 17)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "VolleyBalls:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Times New Roman", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(33, 163)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(86, 17)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "BasketBalls:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Times New Roman", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(33, 115)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(70, 17)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "FootBalls:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Times New Roman", 10.0!, CType(((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic) _
+                Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(452, 64)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(72, 17)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Total Price"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Times New Roman", 10.0!, CType(((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic) _
+                Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(345, 64)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(68, 17)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Item Price"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Times New Roman", 10.0!, CType(((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic) _
+                Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(150, 64)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(117, 17)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Quantity Purchase"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Times New Roman", 10.0!, CType(((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic) _
+                Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(33, 64)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(73, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Item Name"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.DarkCyan
+        Me.ClientSize = New System.Drawing.Size(784, 495)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Name = "Form1"
+        Me.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.Text = "                                                                                 " &
+    "                                 KenSport Sports Sales"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents BTNcheckout As System.Windows.Forms.Button
+    Friend WithEvents Label14 As System.Windows.Forms.Label
+    Friend WithEvents Label13 As System.Windows.Forms.Label
+    Friend WithEvents Label12 As System.Windows.Forms.Label
+    Friend WithEvents Label11 As System.Windows.Forms.Label
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents TXTBasketballs As System.Windows.Forms.TextBox
+    Friend WithEvents TXTVolleyballs As System.Windows.Forms.TextBox
+    Friend WithEvents TXTTotalitems As System.Windows.Forms.TextBox
+    Friend WithEvents TXTFootballs As System.Windows.Forms.TextBox
+    Friend WithEvents BTNExit As System.Windows.Forms.Button
+    Friend WithEvents TXTTax As System.Windows.Forms.TextBox
+    Friend WithEvents TXTTotalsales As System.Windows.Forms.TextBox
+    Friend WithEvents TXTSubtotal As System.Windows.Forms.TextBox
+    Friend WithEvents TXTThirdprice As System.Windows.Forms.TextBox
+    Friend WithEvents TXTSecondprice As System.Windows.Forms.TextBox
+    Friend WithEvents TXTfirstprice As System.Windows.Forms.TextBox
+
+End Class
---
+++ KenSportSportsSales1/KenSportSportsSales/Form1.vb
@@ -0,0 +1,55 @@
+﻿Public Class Form1
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TXTFootballs.TextChanged
+
+    End Sub
+
+    Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles TXTVolleyballs.TextChanged
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TXTTotalitems.TextChanged
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles BTNExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles BTNcheckout.Click
+        '---------------------------------Variable Declaration & Calculation-----------------------------------------------------------------------------------'
+        Dim Footballs As Integer = TXTFootballs.Text
+        Dim Basketballs As Integer = TXTBasketballs.Text
+        Dim Volleyballs As Integer = TXTVolleyballs.Text
+        Dim Totalitems As Integer = Footballs + Basketballs + Volleyballs
+        Dim Priceone As Integer = 44 * Footballs
+        Dim Pricetwo As Integer = 49 * Basketballs
+        Dim Pricethree As Integer = 39 * Volleyballs
+        Dim Subtotal As Integer = Priceone + Pricetwo + Pricethree
+        Dim Tax As Decimal = (5 / 100) * Subtotal
+        Dim TotalSales As Decimal = Subtotal + Tax
+        '-------------------------------------Output Display---------------------------------------------------------------------------------------------------------------'
+        TXTTotalitems.Text = Totalitems.ToString
+        TXTfirstprice.Text = Priceone.ToString
+        TXTSecondprice.Text = Pricetwo.ToString
+        TXTThirdprice.Text = Pricethree.ToString
+        TXTSubtotal.Text = Subtotal.ToString
+        TXTTax.Text = Tax.ToString
+        TXTTotalsales.Text = TotalSales.ToString
+
+
+
+    End Sub
+
+    Private Sub TXT44_TextChanged(sender As Object, e As EventArgs) Handles TXTfirstprice.TextChanged
+
+    End Sub
+End Class
